fc86614f61e3fc4ca1546fc24e00f992dadfdc51
Dibyendu Saha <dip4012@gmail.com>
2024-11-09 19:06:43 +0530

    initial commit

---
+++ data/schema.js
@@ -0,0 +1,45 @@
+import { buildSchema } from "graphql"
+
+const schema = buildSchema(`   
+    type Store {
+        store: String
+    }
+
+    enum Soldout {
+        SOLDOUT,
+        ONSALE
+    }
+
+    type Product {
+        id: ID,
+        name: String,
+        description: String,
+        price: Float,
+        soldout: Soldout,
+        inventory: Int
+        stores: [Store]!
+    }
+
+    type Query {
+        getProduct(id: ID): Product
+    }
+
+    input StoreInput {
+        store: String
+    }
+
+    input ProductInput {
+        name: String,
+        description: String,
+        price: Float,
+        soldout: Soldout,
+        inventory: Int
+        stores: [StoreInput]!
+    }
+
+    type Mutation {
+        createProduct(input: ProductInput): Product
+    }
+`)
+
+export default schema
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import schema from "./data/schema"
+import resolvers from "./data/resolvers"
+
+const PORT = 8080
+
+const app = express()
+
+app.get("/", (req, res) => {
+	res.send("GraphQL is amazing!")
+})
+
+const root = resolvers
+
+app.use(
+	"/graphQL",
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true,
+	})
+)
+
+app.listen(PORT, () =>
+	console.log(`Running server on http://localhost:${PORT}/graphQL`)
+)
